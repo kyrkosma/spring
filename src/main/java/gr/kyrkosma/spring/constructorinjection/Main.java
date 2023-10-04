92b5369c3e29ab882e45676cfc3129128153eeba
kyrkosma <kyrkosma@gmail.com>
2023-10-04 22:41:04 +0200

    Constructor injection and Spring version upgrade - Spring

---
+++ src/main/java/gr/kyrkosma/spring/constructorinjection/Main.java
@@ -0,0 +1,15 @@
+package gr.kyrkosma.spring.constructorinjection;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("constructorinjection.xml");
+        Employee employee = (Employee) context.getBean("employee");
+        System.out.println(employee);
+
+        Employee employee123 = (Employee) context.getBean("employee123");
+        System.out.println(employee123);
+    }
+}
